56fc30dbf0abe949420178e4e6e71f7565873a88
nitinrajput96 <nitin965432@gmail.com>
2024-12-23 22:53:22 +0530

    update

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -24,7 +24,7 @@ export const Header = () => {
       <div className=' max-[400px]:h-[70px] w-full  h-24 bg-gradient-to-t from-zinc-200 to-indigo-400 shadow-xl flex  justify-between items-center  px-1'>
          
          {/* logo */}
-         <div className='  max-[400px]:w-32 max-[400px]:h-11 max-[400px]:text-[14px] max-[400px]:font-extrabold w-auto p-1 h-11 bg-transparent text-blue-800 font-mono text-2xl font-extrabold  flex justify-center items-center border rounded-md border-blue-700'>
+         <div className='  max-[400px]:w-3/6 max-[400px]:h-11 max-[400px]:text-[14px] max-[400px]:font-extrabold w-auto p-1 h-11 bg-transparent text-blue-800 font-mono text-2xl font-extrabold  flex justify-center items-center border rounded-md border-blue-700'>
              Booking-hotels
          </div>
 
