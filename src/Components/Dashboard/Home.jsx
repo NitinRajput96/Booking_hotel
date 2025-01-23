0078e0ca2d364bcaf9e92e34fd2fb46d57d5372f
nitinrajput96 <nitin965432@gmail.com>
2025-01-23 19:19:57 +0530

    update

--- src/Components/Dashboard/Home.jsx
+++ src/Components/Dashboard/Home.jsx
@@ -9,11 +9,17 @@ export const Home = () => {
     <div className=' w-full h-full grid max-[400px]:grid-cols-1 grid-cols-4 justify-items-center p-1
      gap-2'>{
       Hotel_data.hotelDetails.map((item,i)=>
-          <>
-            <img className=' w-96 h-72 border   ' src={item.img} alt="" />
-            <span className=' font-mono text-md text-black font-bold'>{item.hote_name}</span>
+          <div className='w-auto h-auto flex  flex-col justify-center items-center m-2 rounded-md bg-gray-100 p-2 gap-2'>
 
-          </>        
+            <img className=' w-96 h-72 border rounded-md   ' src={item.img} alt="" />
+            <div className=' w-72 font-mono text-md text-black font-bold flex flex-col  '>
+                  {item.hote_name}
+                  {item.hot_city}
+                  {item.hot_location}
+                  {item.hot_reviow}
+                  
+            </div>
+          </div>        
       )
     }</div>
   )
