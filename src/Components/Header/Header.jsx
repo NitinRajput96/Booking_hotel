fc6b8f6f5201529d05e8d42c0546a03fd51a5702
nitinrajput96 <nitin965432@gmail.com>
2025-01-28 18:37:49 +0530

    update

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -52,7 +52,7 @@ export const Header = () => {
                   <div className=' flex justify-center items-center  text-lg text-white py-2  min-[450px]:hidden'>
                         <FaRegUserCircle />
                   </div>
-                  <div className=' flex justify-center items-center  text-lg text-white py-2  min-[450px]:hidden'>
+                  <div className=' flex justify-center items-center  text-lg text-white py-2  min-[450px]:'>
                   <FaListCheck onClick={()=>{setState(!state)}}/>
                   </div> 
             </div>
