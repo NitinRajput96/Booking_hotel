5d4f011895fa6062acfc7fb3d64314e8c1fcb9ff
nitinrajput96 <nitin965432@gmail.com>
2025-01-31 18:47:30 +0530

    update

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -33,7 +33,7 @@ export const Header = () => {
 
   return (
    <>
-    <div className=' w-full  h-[70px] fixed  top-0  flex justify-center items-center  bg-indigo-600  z-50 '>
+    <div className=' w-full  h-[70px] fixed  top-0  flex justify-center items-center  bg-indigo-600  z-10 '>
     <div className='  max-[450px]:px-2  w-11/12 h-full   bg-indigo-600  flex  justify-between items-center border-b '>
          
          {/* logo */}
@@ -47,7 +47,7 @@ export const Header = () => {
               <Link className=' w-12   hover:bg-indigo-500 hover:rounded-md h-11  flex justify-center items-center'>
                  {
                    Hotel_data.logo.map((item,i)=>
-                     <img className=' w-7 h-6' src={item.logoFlage} alt="" />
+                     <img className=' w-8 h-6' src={item.logoFlage} alt="" />
                    )
                  }</Link>
 
@@ -91,7 +91,7 @@ export const Header = () => {
       {/* sidenavbar */}
      {
         state===true?
-        <div className=' w-full h-full bg-indigo-600  z-40 top-0  absolute  pb-14 px-3  text-left border min-[450px]:hidden'>
+        <div className='trans w-full h-full bg-indigo-600  z-50 top-0  absolute  pb-14 px-3  text-left border min-[450px]:hidden'>
       
       <div className=' w-12 h-12 absolute top-0 right-0  z-50 flex justify-center items-center text-[30px]  text-black'>
       <MdOutlineCancel onClick={()=>{setState(!state)}} />
@@ -116,7 +116,7 @@ export const Header = () => {
 
 
 
-     <div className=' w-full h-auto  fixed top-16 z-50  bg-indigo-600 flex justify-center   '>
+     <div className=' w-full h-auto  fixed top-16 z-10  bg-indigo-600 flex justify-center   '>
          <div className=' w-11/12 md:h-16  bg-indigo-600  flex justify-start items-center py-4 min-[450]:gap-4 gap-2 flex-wrap '>
             <Link to="/" className='  py-1 px-2 sm:py-[1px] sm:[1px] max-[450px]:text-[12px] max-[450px]:px-1 flex justify-center items-center   hover:border  gap-1 text-white  rounded-lg ' ><MdNightsStay/>Stays</Link>
             <Link to="/" className='  py-1 px-2 sm:py-[1px] sm:[1px] max-[450px]:text-[12px] max-[450px]:px-1 flex justify-center items-center   hover:border  gap-1 text-white  rounded-lg ' ><PiAirplaneTakeoffLight/>Flights</Link>
