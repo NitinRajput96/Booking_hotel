e63913909a6c5705eda57dca0c82697b271095bd
nitinrajput96 <nitin965432@gmail.com>
2025-02-02 19:12:45 +0530

    update

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const Footer = () => {
   return (
     <>
-      <div className=' w-full h-32  text-center text-black font-semibold    bg-indigo-600'>
+      <div className=' w-full h-32  bottom-0 text-center text-black font-semibold    bg-indigo-600'>
           <h4>Footer</h4>
           <span className=' text-sm'>Nitin Rajput </span>
       </div>
