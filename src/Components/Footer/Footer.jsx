c465777ef12fc626c898f99e3e4216f2fc3704e3
nitinrajput96 <nitin965432@gmail.com>
2025-02-02 19:23:45 +0530

    update

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -5,7 +5,7 @@ export const Footer = () => {
     <>
       <div className=' w-full h-36 flex items-center  flex-col flex-wrap  bottom-0 text-center text-black font-semibold    bg-indigo-600'>
       
-          <span className=' w-11/12 underline p-1 border-b text-[12px] '>Booking-hotels</span>
+          <span className=' w-11/12 underline p-1 border-b text-[12px] text-white font-semibold '>Booking-hotels</span>
          
           
       </div>
