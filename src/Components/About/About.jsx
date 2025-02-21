3c290ec4d5d9c8c33acbd3fcfce2ac36a42467cc
nitinrajput96 <nitin965432@gmail.com>
2025-02-21 14:21:22 +0530

    updatere

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -12,8 +12,8 @@ export const About = () => {
     <h3>Rent Cars</h3>
     <div className='w-full h-auto bg-gray-100 p-3 rounded-md flex justify-start items-center gap-5 flex-col'>
          {
-          Cars.CarsandBikes.map((item,i)=>
-             <div className=' w-full h-64  shadow-sm rounded-md flex flex-col justify-center items-center'>
+          Cars.CarsandBikes.map((item)=>
+             <div className=' w-full h-64  shadow-sm rounded-md flex flex-col justify-center items-center' key={item.i}>
                       <img src={item.img} className=' w-full p-5 h-5/6 bg-violet-100 rounded-md ' alt="" />
                       <div className=' w-full h-1/6'>{item.id}</div>
                       <div>{item.carName}</div>
