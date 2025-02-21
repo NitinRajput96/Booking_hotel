537a5426872ea21ad59a37a04d0d51a9a185c955
nitinrajput96 <nitin965432@gmail.com>
2025-02-21 13:54:17 +0530

    updatecars

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,7 +1,27 @@
 import React from 'react'
+import Cars from "../Json-data/Cars.json"
+import Item from 'antd/es/list/Item'
+
 
 export const About = () => {
+  
+     
+
   return (
-    <div>About</div>
+   <>
+    <h3>Rent Cars</h3>
+    <div className='w-full h-auto bg-gray-100 p-3 rounded-md flex justify-start items-center gap-5 flex-col'>
+         {
+          Cars.CarsandBikes.map((item,i)=>
+             <div className=' w-full h-64  shadow-sm rounded-md flex flex-col justify-center items-center'>
+                      <img src={item.img} className=' w-full p-5 h-5/6 bg-violet-100 rounded-md ' alt="" />
+                      <div className=' w-full h-1/6'>{item.id}</div>
+                      <div>{item.carName}</div>
+
+             </div>
+          )
+         }
+    </div>
+   </>
   )
 }
