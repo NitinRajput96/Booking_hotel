84b31876e041c1dd15891379c54c107906cb5967
nitinrajput96 <nitin965432@gmail.com>
2025-02-25 12:15:03 +0530

    update

--- src/Components/carRentls/ViewCars.jsx
+++ src/Components/carRentls/ViewCars.jsx
@@ -20,20 +20,23 @@ export const ViewCars = () => {
          
   return (
     <>
-        <div className=' w-full h-auto my-2 text-[20px] font-semibold px-6 flex justify-center items-center '>{viewState}  <span className=' p-2  text-orange-600 font-semibold '>{vehical} </span></div>
+        <div className=' w-full h-full text-[20px] font-semibold  flex justify-center items-center  '>{viewState}  <span className=' p-2  text-orange-600 font-semibold '>{vehical} </span></div>
 
-        <div className=' w-full  h-full flex justify-center items-center flex-col gap-4 py-5 '>
+        <div className=' w-full h-full flex justify-center items-center p-5 bg-gray-50 '>
             {
                 Cars.CarsandBikes.map((item,i)=>{
                     return item.carName===viewState?<>
-                        {
+                        { 
                            <>
-                          <div className=' w-full h-56 flex justify-center items-center '>
-                             <img src={item.img} className=' w-11/12 h-11/12' alt="" />
-                          </div>
+                             <div className=' w-full h-full gap-7 bg-white rounded-md shadow-sm  grid grid-rows-2 grid-flow-row justify-items-center items-center'>
+                                    <div className=' w-full  '>
+                                        <img src={item.img} className=' w-full h-full' alt="" />
+                                    </div>
 
-                           <div className='w-full h-auto mb-2  p-7 text-[13px] font-sans font-semibold text-gray-600 '>{item.about}</div>
-
+                                    <div className='w-full  h-full  mb-2  p-7 text-[13px] font-sans font-semibold text-gray-600 '>
+                                        {item.about}
+                                    </div>
+                             </div> 
                            </>
                             
                         }
