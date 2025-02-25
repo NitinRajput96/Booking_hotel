a1de9e3449e91345cd279375f7ee31afc829003b
nitinrajput96 <nitin965432@gmail.com>
2025-02-25 12:26:44 +0530

    update

--- src/Components/carRentls/ViewCars.jsx
+++ src/Components/carRentls/ViewCars.jsx
@@ -21,15 +21,15 @@ export const ViewCars = () => {
   return (
     <>
 
-        <div className=' w-full h h-lvh flex justify-start items-center flex-col p-5 bg-gray-50 '>
-        <div className=' w-full h- text-[20px] font-semibold  flex justify-center items-center  '>{viewState}  <span className=' p-2  text-orange-600 font-semibold '>{vehical} </span></div>
+        <div className=' w-full h h-full flex justify-start items-center flex-col p-5 bg-gray-50 '>
+        <div className=' w-full  h-16 bg-indigo-400 rounded-sm mb-2 text-[22px] font-semibold  flex justify-center items-center  '>{viewState}  <span className=' p-2  text-orange-600 font-semibold '>{vehical} </span></div>
 
             {
                 Cars.CarsandBikes.map((item,i)=>{
                     return item.carName===viewState?<>
                         { 
                            <>
-                             <div className=' w-full h-full gap-7 bg-white rounded-md shadow-sm  grid grid-rows-2 grid-flow-row justify-items-center items-center'>
+                             <div className=' w-full h-5/6 gap-7 bg-white rounded-md shadow-sm  grid grid-rows-2 grid-flow-row justify-items-center items-center'>
                                     <div className=' w-full  '>
                                         <img src={item.img} className=' w-full h-full' alt="" />
                                     </div>
