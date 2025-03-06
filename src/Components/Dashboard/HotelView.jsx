478d9931b5dbf903bd37be55157ff674def71bf3
nitinrajput96 <nitin965432@gmail.com>
2025-03-06 23:19:54 +0530

    update

--- src/Components/Dashboard/HotelView.jsx
+++ src/Components/Dashboard/HotelView.jsx
@@ -11,26 +11,14 @@ import toast, { Toaster } from 'react-hot-toast';
 export const HotelView = () => {
 
     const {catagory,setCatagory}=useCategory()
-    const [star,setStar]=useState([])
     const [rating,setRating]=useState(0)
      
-    
-
-
-    
-   
-
-    const startPrint=()=>{
-       Hotel_data.hotelDetails.filter((item)=> {return item.hote_name===catagory?setStar(item.stars):""})
-    }
-
     const ratingFunk =()=>{
          rating>0?toast.success("You rate "+rating+" Stars"):""
     }
   
        
     useEffect(() => {
-      startPrint()
       ratingFunk()
     }, [rating])
    
@@ -43,39 +31,46 @@ export const HotelView = () => {
    <>
    <Toaster/>
      <div className=' w-full h-full flex justify-start items-center flex-col  p-2'>
+    
      
            {
             Hotel_data.hotelDetails.map((item,i)=>{
                 return item.hote_name===catagory?<>
-                     <div className=' w-full h-full  p-1' key={i} >
-                          <div className=' w-full h-auto flex  justify-between items-center my-2'>
-                             <span className=' flex justify-center items-center  gap-[2px]'>
-                             {
-                              star.map((item)=>{
-                                return <>
-                                <span className=' '
-                                         key={item}
-                                         onClick={()=>(setRating(item))}
-                                         style={{
-                                           cursor:"pointer",
-                                           fontSize:"18px",
-                                           color:item<=rating?"gold":"gray"
-                                         }}
-                                        >
-                                           <IoStar/> 
-                                       </span>
-                                </>
-                              }) 
-                            }
-                             Rating
-                             </span>
-                            
-                            
-                            <span className=' text-[20px] text-indigo-600'>
-                                 <IoShareSocialSharp/>
-                            </span>
-                            
-                          </div>
+                 <div className=' w-full h-full  p-1' key={i} >
+                     <div className=' w-full h-auto flex  justify-between items-center my-2'>
+                                  <span className=' flex justify-center items-center  gap-[2px]'>
+                                  {
+                                    item.stars!=null?
+                                    <>
+                                         {
+                                            item.stars.map((item)=>{
+                                            return<>
+                                              <span className=' '
+                                                        key={item}
+                                                        onClick={()=>(setRating(item))}
+                                                        style={{
+                                                          cursor:"pointer",
+                                                          fontSize:"18px",
+                                                          color:item<=rating?"gold":"gray"
+                                                        }}
+                                                      >
+                                                          <IoStar/> 
+                                              </span>
+                                            </>
+                                      }) 
+                                         }
+                                         <span className=' text-sm'>Rating..</span>
+                                    </>:""
+                                  }
+                                  </span>
+                                
+                                
+                                <span className=' text-[20px] text-indigo-600'>
+                                      <IoShareSocialSharp/>
+                                </span>
+                                
+                              </div>
+                         
                         <div className=' w-full h-auto  text-lg   font-bold '>{item.hote_name}</div>
                             <div className=' w-full h-3/6 flex justify-center items-center '>
                                   <img src={item.img} className=' w-full h-full rounded-md  shadow' alt="" />
